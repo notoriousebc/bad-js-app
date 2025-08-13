4f4eec09715a74ecab4e1c1ceeb7c769912eab64
notoriousebc <ellabc@gmail.com>
2025-08-13 16:14:09 -0600

    Update likeProductReviews.ts
    express-mongo-nosqli

--- routes/likeProductReviews.ts
+++ routes/likeProductReviews.ts
@@ -1,4 +1,56 @@
-  // At the top of your file, add the following imports
+// Add the following import at the top of your file, if it's not already present
+            const { ObjectId } = require('mongodb');
+
+            // ...the rest of your file...
+
+            module.exports = function productReviews () {
+              return (req: Request, res: Response, next: NextFunction) => {
+                // Validate and convert the incoming id to a MongoDB ObjectId
+                const id = ObjectId.isValid(req.body.id) ? new ObjectId(req.body.id) : null;
+                if (!id) {
+                  return res.status(400).json({ error: 'Invalid ID format' });
+                }
+                const user = security.authenticatedUsers.from(req)
+                db.reviews.findOne({ _id: id }).then((review: Review) => {
+                  if (!review) {
+                    res.status(404).json({ error: 'Not found' })
+                  } else {
+                    const likedBy = review.likedBy
+                    if (!likedBy.includes(user.data.email)) {
+                      db.reviews.update(
+                        { _id: id },
+                        { $inc: { likesCount: 1 } }
+                      ).then(
+                        () => {
+                          // Artificial wait for timing attack challenge
+                          setTimeout(function () {
+                            db.reviews.findOne({ _id: id }).then((review: Review) => {
+                              const likedBy = review.likedBy
+                              likedBy.push(user.data.email)
+                              let count = 0
+                              for (let i = 0; i < likedBy.length; i++) {
+                                if (likedBy[i] === user.data.email) {
+                                  count++
+                                }
+                              }
+                              challengeUtils.solveIf(challenges.timingAttackChallenge, () => { return count > 2 })
+                              db.reviews.update(
+                                { _id: id },
+                                { $set: { likedBy: likedBy } }
+                              ).then(
+                                (result: any) => {
+                                  res.json(result)
+                                }, (err: unknown) => {
+                                  res.status(500).json(err)
+                                })
+                            }, () => {
+                              res.status(400).json({ error: 'Wrong Params' })
+                            })
+                          }, 150)
+                        }, (err: unknown) => {
+                          res.status(500).json(err)
+
+// At the top of your file, add the following imports
     import mongoSanitize = require('mongo-sanitize');
     import { ObjectId } from 'mongodb';
 
